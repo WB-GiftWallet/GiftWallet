c20c0a072087976c2746cc911cfbc8c58072662e
Dylan-Yoon <d.dylany21@gmail.com>
2023-02-19 16:38:15 +0900

    build: Project Setting

---
+++ GiftWallet/GiftWallet/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  GiftWallet
+//
+//  Created by WB on 2023/02/19.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
---
+++ GiftWallet/GiftWallet/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GiftWallet
+//
+//  Created by WB on 2023/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
