8c8a2507a1d76dcf57c4540d9f5835dd14dfa733
woong <workplayhard1@naver.com>
2023-02-28 19:48:44 +0900

    feat: AllGifts 데이터를 Sorting 해주는 함수를 UseCase로 타입분리 및 테스트코드 작성

---
+++ GiftWallet/GiftWallet/UseCase/DateCalCulateUseCase.swift
@@ -0,0 +1,67 @@
+//
+//  DateCalCulateUseCase.swift
+//  GiftWallet
+//
+//  Created by 서현웅 on 2023/02/28.
+//
+
+import Foundation
+
+// MARK: 외부함수
+struct DateCalculateUseCase {
+    func sortOutExpireDate(_ observableGifts: Observable<[Gift]>,
+                           _ gifts: [Gift]) {
+        observableGifts.value = gifts.filter({ gift in
+            guard let expireDate = gift.expireDate else { return false }
+            return checkExpireDateIsSmallerThanSevenDays(expireDate: expireDate)
+        })
+    }
+    
+    func sortOutRecentDate(_ observableGifts: Observable<[Gift]>,
+                           _ gifts: [Gift]) {
+        observableGifts.value = gifts.filter({ gift in
+            guard let expireDate = gift.expireDate else { return true }
+            return checkExpireDateIsBiggerThanSevenDays(expireDate: expireDate)
+        })
+    }
+}
+
+// MARK: 내부함수
+extension DateCalculateUseCase {
+    func checkExpireDateIsSmallerThanSevenDays(expireDate: Date) -> Bool {
+        let subtractionResult = subtractionOftheDays(expireDate: expireDate, today: Date())
+        
+        return subtractionResult <= 7 ? true : false
+    }
+    
+    func checkExpireDateIsBiggerThanSevenDays(expireDate: Date) -> Bool {
+        let subtractionResult = subtractionOftheDays(expireDate: expireDate, today: Date())
+        
+        return subtractionResult > 7 ? true : false
+    }
+    
+    func subtractionOftheDays(expireDate: Date, today: Date) -> Int {
+        let calendar = Calendar.current
+        let formattedExpireDate = setupDateFormat(expireDate: expireDate, today: today).0
+        let formattedToday = setupDateFormat(expireDate: expireDate, today: today).1
+        
+        let components = calendar.dateComponents([.day], from: formattedToday, to: formattedExpireDate)
+        
+        return components.day ?? 0
+    }
+    
+    private func setupDateFormat(expireDate: Date, today: Date) -> (Date, Date) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        let expireDateString = dateFormatter.string(from: expireDate)
+        let todayString = dateFormatter.string(from: today)
+        
+        guard let expireDate = dateFormatter.date(from: expireDateString),
+              let today = dateFormatter.date(from: todayString) else {
+            return (Date(), Date())
+        }
+        
+        return (expireDate: expireDate, today: today)
+    }
+}
