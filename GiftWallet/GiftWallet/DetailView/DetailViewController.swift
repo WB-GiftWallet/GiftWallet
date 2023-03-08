2400883fa3993f7773b8201f09e22fd28fba6596
woong <workplayhard1@naver.com>
2023-03-08 16:40:07 +0900

    feat: 사용성개선작업(3) - 기존의 DetailViewController의 UI 컴포넌트를 PagingCollectionViewCell로 이동시킴

--- GiftWallet/GiftWallet/DetailView/DetailViewController.swift
+++ GiftWallet/GiftWallet/DetailView/DetailViewController.swift
@@ -21,63 +21,6 @@ final class DetailViewController: UIViewController {
         return collectionView
     }()
     
-    private let brandLabel: UILabel = {
-        let label = UILabel()
-        
-        label.font = .preferredFont(forTextStyle: .title1)
-        label.numberOfLines = 1
-        
-        return label
-    }()
-    
-    private let productNameLabel: UILabel = {
-        let label = UILabel()
-        
-        label.font = .preferredFont(forTextStyle: .title2)
-        label.numberOfLines = 1
-        
-        return label
-    }()
-    
-    private let dateDueLabel: UILabel = {
-        let label = UILabel()
-        
-        label.font = .preferredFont(forTextStyle: .title2)
-        label.numberOfLines = 1
-        
-        return label
-    }()
-        
-    private let memoTextField: UITextField = {
-        let textField = UITextField()
-        
-        textField.font = .preferredFont(forTextStyle: .title2)
-        textField.borderStyle = .roundedRect
-        
-        return textField
-    }()
-    
-    private let selectedButton: UIButton = {
-        let button = UIButton()
-        
-        button.setTitle("사용 완료", for: .normal)
-        button.backgroundColor = .systemPurple
-        button.layer.cornerRadius = 5
-        button.addTarget(nil,
-                         action: #selector(tapSeletedButton),
-                         for: .touchUpInside)
-        
-        return button
-    }()
-    
-    private let giftImageView: UIImageView = {
-        let imageView = UIImageView()
-        
-        imageView.contentMode = .scaleAspectFit
-        
-        return imageView
-    }()
-    
     init(viewModel: DetailViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -89,30 +32,30 @@ final class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureImageTapGesture()
+//        configureImageTapGesture()
         setupNavigation()
         setupViews()
-        configureGiftData()
+//        configureGiftData()
     }
     
-    private func configureGiftData() {
-        brandLabel.text = viewModel.brandName
-        productNameLabel.text = viewModel.productName
-        dateDueLabel.text = viewModel.expirdDate
-        memoTextField.text = viewModel.memo
-        giftImageView.image = viewModel.gift.image
-    }
+//    private func configureGiftData() {
+//        brandLabel.text = viewModel.brandName
+//        productNameLabel.text = viewModel.productName
+//        dateDueLabel.text = viewModel.expirdDate
+//        memoTextField.text = viewModel.memo
+//        giftImageView.image = viewModel.gift.image
+//    }
     
     private func setupNavigation() {
         navigationController?.navigationBar.tintColor = .black
     }
     
-    private func configureImageTapGesture() {
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapImageView))
-        
-        giftImageView.isUserInteractionEnabled = true
-        giftImageView.addGestureRecognizer(gestureRecognizer)
-    }
+//    private func configureImageTapGesture() {
+//        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapImageView))
+//
+//        giftImageView.isUserInteractionEnabled = true
+//        giftImageView.addGestureRecognizer(gestureRecognizer)
+//    }
     
     @objc private func tapSeletedButton() {
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
@@ -123,44 +66,27 @@ final class DetailViewController: UIViewController {
         }
         
         let done = UIAlertAction(title: "네", style: .default) { _ in
-            self.doneAction()
+//            self.doneAction()
         }
 
         [cancel, done].forEach(alert.addAction(_:))
         present(alert, animated: true)
     }
     
-    private func doneAction() {
-        changeGiftState()
-        viewModel.writeMemo(memoTextField.text)
-        viewModel.coreDataUpdate()
-    }
+//    private func doneAction() {
+//        changeGiftState()
+//        viewModel.writeMemo(memoTextField.text)
+//        viewModel.coreDataUpdate()
+//    }
     
-    private func changeGiftState() {
-        viewModel.toggleToUnUsableState()
-        selectedButton.backgroundColor = .systemGray
-        dismiss(animated: true)
-    }
-        
-    @objc private func tapImageView(sender: UITapGestureRecognizer) {
-        let gift = viewModel.gift
-        
-        let viewModel = GiftImageViewModel(gift: gift)
-        let giftImageViewController = GiftImageViewController(viewModel: viewModel)
-        giftImageViewController.modalPresentationStyle = .fullScreen
-        present(giftImageViewController, animated: true)
-    }
-    
-}
+//    private func changeGiftState() {
+//        viewModel.toggleToUnUsableState()
+//        selectedButton.backgroundColor = .systemGray
+//        dismiss(animated: true)
+//    }
 
-// MARK: AutoLayout 관련 메서드
-extension DetailViewController {
     private func setupViews() {
         view.backgroundColor = .systemBackground
-        
-//        configureScrollView()
-//        configureContentView()
-//        configureInnerContents()
         
         view.addSubview(pagingCollectionView)
         
@@ -173,40 +99,33 @@ extension DetailViewController {
         
     }
     
-    
-    private func configureInnerContents() {
-        [brandLabel, productNameLabel, dateDueLabel, memoTextField, selectedButton, giftImageView].forEach {
-            $0.translatesAutoresizingMaskIntoConstraints = false
-            contentsView.addSubview($0)
-        }
+    @objc private func tapImageView(sender: UITapGestureRecognizer) {
+        let gift = viewModel.gift
         
-        NSLayoutConstraint.activate([
-            brandLabel.topAnchor.constraint(equalTo: contentsView.topAnchor, constant: 10),
-            brandLabel.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor, constant: view.frame.width / 20),
-            brandLabel.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor, constant: -view.frame.width / 20),
-
-            productNameLabel.topAnchor.constraint(equalTo: brandLabel.bottomAnchor, constant: 10),
-            productNameLabel.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor, constant: view.frame.width / 20),
-            productNameLabel.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor, constant: -view.frame.width / 20),
-
-            dateDueLabel.topAnchor.constraint(equalTo: productNameLabel.bottomAnchor, constant: 5),
-            dateDueLabel.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor, constant: view.frame.width / 20),
-            dateDueLabel.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor, constant: -view.frame.width / 20),
-
-            memoTextField.topAnchor.constraint(equalTo: dateDueLabel.bottomAnchor, constant: 10),
-            memoTextField.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor, constant: view.frame.width / 20),
-            memoTextField.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor, constant: -view.frame.width / 20),
-            
-            selectedButton.topAnchor.constraint(equalTo: memoTextField.bottomAnchor, constant: 20),
-            selectedButton.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor, constant: view.frame.width / 20),
-            selectedButton.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor, constant: -view.frame.width / 20),
-            selectedButton.heightAnchor.constraint(equalToConstant: view.frame.height / 20),
-
-            giftImageView.topAnchor.constraint(equalTo: selectedButton.bottomAnchor, constant: view.frame.width / 20),
-            giftImageView.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor),
-            giftImageView.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor),
-            giftImageView.bottomAnchor.constraint(equalTo: contentsView.bottomAnchor),
-            giftImageView.heightAnchor.constraint(lessThanOrEqualTo: giftImageView.widthAnchor, multiplier: 2)
-        ])
+        let viewModel = GiftImageViewModel(gift: gift)
+        let giftImageViewController = GiftImageViewController(viewModel: viewModel)
+        giftImageViewController.modalPresentationStyle = .fullScreen
+        present(giftImageViewController, animated: true)
     }
+    
+}
+
+// MARK: AutoLayout 관련 메서드
+extension DetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = pagingCollectionView.dequeueReusableCell(withReuseIdentifier: PagingCollectionViewCell.reuseIdentifier,
+                                                            for: indexPath)
+        
+        return cell
+    }
+    
+
+}
+
+extension DetailViewController: UICollectionViewDelegate {
+    
 }
--- GiftWallet/GiftWallet/DetailView/PagingCollectionViewCell.swift
+++ GiftWallet/GiftWallet/DetailView/PagingCollectionViewCell.swift
@@ -25,6 +25,56 @@ class PagingCollectionViewCell: UICollectionViewCell, ReusableView {
         return view
     }()
     
+    private let brandLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = UIFont(style: .bold, size: 25)
+        
+        return label
+    }()
+    
+    private let productNameLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = UIFont(style: .medium, size: 20)
+        
+        return label
+    }()
+    
+    private let dateDueLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = UIFont(style: .light, size: 19)
+        
+        return label
+    }()
+    
+    private let memoTextField: CustomTextField = {
+        let textField = CustomTextField()
+        
+        return textField
+    }()
+    
+    private lazy var button: UIButton = {
+        
+        button.setTitle("사용 완료", for: .normal)
+        button.backgroundColor = .systemPurple
+        button.layer.cornerRadius = 5
+//        button.addTarget(nil,
+//                         action: #selector(tapSeletedButton),
+//                         for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private let giftImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -49,7 +99,7 @@ class PagingCollectionViewCell: UICollectionViewCell, ReusableView {
             scrollView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             scrollView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-        
+            
             containterView.topAnchor.constraint(equalTo: contentLayoutGuide.topAnchor),
             containterView.leadingAnchor.constraint(equalTo: contentLayoutGuide.leadingAnchor),
             containterView.trailingAnchor.constraint(equalTo: contentLayoutGuide.trailingAnchor),
