4c53ce1c79888ce7b139bee9cd7612e6aa6df63b
Dylan-Yoon <d.dylany21@gmail.com>
2023-03-08 23:27:41 +0900

    feat: SearchViewController 추천검색을 위한 재 구현

---
+++ GiftWallet/GiftWallet/SearchView/SearchViewController.swift
@@ -0,0 +1,166 @@
+//
+//  SearchViewController.swift
+//  GiftWallet
+//
+//  Created by Baem on 2023/03/07.
+//
+
+import UIKit
+
+final class SearchViewController: UIViewController {
+    
+    private let searchBar = UISearchBar(frame: .zero)
+    
+    private let giftSearchController = UISearchController(searchResultsController: nil)
+    
+    private var allGiftData = [Gift]()
+    private var filteringGifts = [Gift]()
+    private var isFiltering: Bool {
+        let searchController = self.navigationItem.searchController
+        let isActive = searchController?.isActive ?? false
+        let isSearchBarHasText = searchController?.searchBar.text?.isEmpty == false
+        return isActive && isSearchBarHasText
+    }
+    
+    private let scrollView = UIScrollView()
+    private let recommendView = RecommendView()
+    
+    private let searchTableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+        fetchGiftCoreData()
+        setSearchController()
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(recommendView)
+        
+        view.addSubview(searchTableView)
+        searchTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        recommendView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsHorizontalScrollIndicator = false
+        
+        NSLayoutConstraint.activate([
+            //scrollView
+            scrollView.frameLayoutGuide.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.frameLayoutGuide.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.frameLayoutGuide.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.frameLayoutGuide.heightAnchor.constraint(equalTo: recommendView.heightAnchor),
+            
+            recommendView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            recommendView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            recommendView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            recommendView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            
+            searchTableView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            searchTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            searchTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            searchTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    private func fetchGiftCoreData() {
+        
+        switch CoreDataManager.shared.fetchData() {
+            case .success(let data):
+                data.forEach { giftData in
+                    allGiftData.append(giftData)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+        }
+        
+        self.filteringGifts = self.allGiftData.sorted(by: {$0.number < $1.number})
+    }
+    
+    private func setSearchController() {
+        
+        giftSearchController.searchResultsUpdater = self
+        
+        giftSearchController.searchBar.placeholder = "브랜드 이름으로 검색하세요!"
+        giftSearchController.hidesBottomBarWhenPushed = true
+        giftSearchController.searchBar.showsSearchResultsButton = true
+        
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+        self.navigationItem.searchController = giftSearchController
+        
+        searchTableView.register(CustomCell.self, forCellReuseIdentifier: "giftCustomCell")
+    }
+    
+}
+
+extension SearchViewController: UITableViewDelegate {
+    
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.isFiltering ? self.filteringGifts.count : self.allGiftData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "giftCustomCell", for: indexPath) as? CustomCell else {
+            return UITableViewCell()
+        }
+        
+        if self.isFiltering {
+            let indexGiftsData = filteringGifts[indexPath.row]
+            cell.giftImageView.image = indexGiftsData.image
+            cell.brandNameLabel.text = indexGiftsData.brandName
+            cell.productNameLabel.text = indexGiftsData.productName
+            cell.expireDateLabel.text = indexGiftsData.expireDate?.setupDateStyleForDisplay()
+        } else {
+            let indexGiftsData = allGiftData[indexPath.row]
+            cell.giftImageView.image = indexGiftsData.image
+            cell.brandNameLabel.text = indexGiftsData.brandName
+            cell.productNameLabel.text = indexGiftsData.productName
+            cell.expireDateLabel.text = indexGiftsData.expireDate?.setupDateStyleForDisplay()
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        var indexGiftsData: Gift
+        
+        if self.isFiltering {
+            indexGiftsData = filteringGifts[indexPath.row]
+        } else {
+            indexGiftsData = allGiftData[indexPath.row]
+        }
+        
+        let detailViewController = DetailViewController(giftData: indexGiftsData)
+        
+        detailViewController.changeBrandLabel(name: indexGiftsData.brandName)
+        detailViewController.changeProductNameLabel(name: indexGiftsData.productName)
+        detailViewController.changeDateDueLabel(date: indexGiftsData.expireDate)
+        detailViewController.changeMemoTextField(name: indexGiftsData.memo)
+        detailViewController.changeGiftImageView(image: indexGiftsData.image)
+        
+        navigationController?.pushViewController(detailViewController, animated: true)
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        return
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.view.frame.height / 5.5
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        guard let text = searchController.searchBar.text else { return }
+        self.filteringGifts = self.allGiftData.filter { $0.brandName!.contains(text) }
+        
+        self.searchTableView.reloadData()
+    }
+}
