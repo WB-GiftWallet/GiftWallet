241fb6a1d48a8e26984cc76cb642bcc90009e3cb
woong <workplayhard1@naver.com>
2023-03-25 13:10:06 +0900

    refactor: SearchViewController Viewmodel을 mainVC에서 주입받도록 수정

--- GiftWallet/GiftWallet/SearchView/SearchViewController.swift
+++ GiftWallet/GiftWallet/SearchView/SearchViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class SearchViewController: UIViewController {
     
-    let viewModel = SearchTableViewModel()
+    let viewModel: SearchTableViewModel
     
     private let searchResultController = SearchTableViewController()
     private lazy var giftSearchController: UISearchController = {
@@ -44,6 +44,15 @@ final class SearchViewController: UIViewController {
         return isActive && isSearchBarHasText
     }
     private var activateConstraint = NSLayoutConstraint()
+    
+    init(viewModel: SearchTableViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
