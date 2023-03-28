7b3effcc82645408c41e68ae3a7941026e9f6b2c
woong <workplayhard1@naver.com>
2023-03-28 23:00:33 +0900

    feat: kakaoButton 추가

--- GiftWallet/GiftWallet/LoginView/LoginViewController.swift
+++ GiftWallet/GiftWallet/LoginView/LoginViewController.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    private let kakaoLoginButton = {
+       let button = UIButton()
+        
+        button.setImage(UIImage(named: "kakao_login_medium_wide"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.addSubview(kakaoLoginButton)
+        
+        NSLayoutConstraint.activate([
+            kakaoLoginButton.widthAnchor.constraint(equalTo: view.widthAnchor),
+            kakaoLoginButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1),
+            kakaoLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            kakaoLoginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
     }
 
 }
