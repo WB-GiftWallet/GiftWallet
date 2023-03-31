fcada9ddb3328ed088bba5d19c66cb843e135bc6
woong <workplayhard1@naver.com>
2023-03-31 16:27:42 +0900

    feat: kakao, apple Login 버튼 추가 및 AuthenticationServices import

--- GiftWallet/GiftWallet/LoginView/LoginViewController.swift
+++ GiftWallet/GiftWallet/LoginView/LoginViewController.swift
@@ -6,10 +6,27 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class LoginViewController: UIViewController {
 
     private let viewModel: LoginViewModel
+    
+    private let kakaoLoginButton = {
+        let button = UIButton()
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    private let appleLoginButton = {
+        let button = ASAuthorizationAppleIDButton(type: .signIn, style: .black)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +35,29 @@ class LoginViewController: UIViewController {
 
     init(viewModel: LoginViewModel) {
         self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setupViews() {
+        [kakaoLoginButton, appleLoginButton].forEach(view.addSubview(_:))
+        
+        NSLayoutConstraint.activate([
+            kakaoLoginButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 30),
+            kakaoLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            kakaoLoginButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            kakaoLoginButton.heightAnchor.constraint(equalTo: kakaoLoginButton.widthAnchor, multiplier: 0.2),
+            
+            appleLoginButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 30),
+            appleLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            appleLoginButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+            appleLoginButton.heightAnchor.constraint(equalTo: appleLoginButton.widthAnchor, multiplier: 0.2),
+            
+        ])
+    }
+    
     
 }
