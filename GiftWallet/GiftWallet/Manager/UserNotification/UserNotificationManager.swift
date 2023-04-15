e5d083bc0bf1d1380733d4134aec6ef5eb4d5ec5
Dylan-Yoon <d.dylany21@gmail.com>
2023-04-15 19:12:21 +0900

    feat: mostRecentExpireItemFetchFromCoreData 구현

--- GiftWallet/GiftWallet/Manager/UserNotification/UserNotificationManager.swift
+++ GiftWallet/GiftWallet/Manager/UserNotification/UserNotificationManager.swift
@@ -20,6 +20,7 @@ class UserNotificationManager {
     }
     
     func requestNotification() {
+        mostRecentExpireItemFetchFromCoreData()
         setContents(contents: notificationContents)
         setDateComponents()
         
@@ -44,13 +45,67 @@ class UserNotificationManager {
         dateComponents.hour = UserDefaults.standard.integer(forKey: "NotificationHour")
         dateComponents.minute = UserDefaults.standard.integer(forKey: "NotificationMinute")
     }
+    
+    
 }
 
-struct userDefualtTimeSetting {
+//MARK: CoreData 데이터 mostRecentExpireDate 가져오기
+extension UserNotificationManager {
+    private func mostRecentExpireItemFetchFromCoreData() {
+        let gifts = fetchFiltedData()
+        var mostExpireDate: Int = 7
+        let dateFormatter = DateFormatter(dateFormatte: DateFormatteConvention.yyyyMMdd)
+        
+        for gift in gifts {
+            guard let expireDate = gift.expireDate else { return }
+            print(judgeGapOfDay(date: expireDate))
+            switch judgeGapOfDay(date: expireDate) {
+                case 0:
+                    mostExpireDate = 0
+                    break
+                case 1...2:
+                    mostExpireDate = min(2, mostExpireDate)
+                case 3...6:
+                    mostExpireDate = min(6, mostExpireDate)
+                default:
+                    continue
+            }
+        }
+    }
     
-    static func timeSetting(hour: Int, minute: Int) {
-        UserDefaults.standard.set(hour, forKey: "NotificationHour")
-        UserDefaults.standard.set(minute, forKey: "NotificationMinute")
+    private func fetchFiltedData() -> [Gift] {
+        var gifts = [Gift]()
+        
+        switch CoreDataManager.shared.fetchData() {
+            case .success(let data):
+                gifts = data.filter { gift in
+                    return gift.useableState
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+        }
+        
+        return gifts
+    }
+    
+    private func judgeGapOfDay(date: Date) -> Int {
+        let formatter = DateFormatter(dateFormatte: DateFormatteConvention.yyyyMMdd)
+        let currentDate = Date()
+        let expireDate = formatter.string(from: date)
+        
+        return Int(ceil(formatter.date(from: expireDate)!.timeIntervalSince(currentDate) / 86400))
     }
 }
 
+extension DateFormatter {
+    convenience init(dateFormatte: DateFormatteConvention) {
+        self.init()
+        self.locale = Locale(identifier: "ko_KR")
+        self.timeZone = TimeZone(abbreviation: "KST")
+        self.dateFormat = dateFormatte.rawValue
+    }
+}
+
+enum DateFormatteConvention: String {
+    case yyyyMMdd
+}
