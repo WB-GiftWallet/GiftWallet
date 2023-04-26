43d109c23b8ddfdba8770cb7bb92db49ba74081a
Dylan-Yoon <d.dylany21@gmail.com>
2023-04-27 01:27:22 +0900

    feat: AlarmCoreDataManager fetch, save 구현 및 sampleCode 구현

--- GiftWallet/GiftWallet/Manager/AlarmCoreDataManager.swift
+++ GiftWallet/GiftWallet/Manager/AlarmCoreDataManager.swift
@@ -5,4 +5,122 @@
 //  Created by Baem on 2023/04/26.
 //
 
-import Foundation
+import CoreData
+import UIKit
+
+final class AlarmCoreDataManager {
+    
+    static let shared = AlarmCoreDataManager()
+    
+    private init() {}
+    
+    private let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    
+    func fetchData() -> Result<[AlarmModel], CoreDataError> {
+        guard let context = appDelegate?.persistentContainer.viewContext else {
+            return .failure(.contextInvalid)
+        }
+        
+        do {
+            let result = try context.fetch(Alarm.fetchRequest())
+            let resultToAlarms = result.compactMap { alarm in
+                AlarmModel(alarm: alarm)
+            }
+            return .success(resultToAlarms)
+        } catch {
+            print(error.localizedDescription)
+        }
+        return .failure(.coreDataError)
+    }
+    
+    func saveData(_ alarm: AlarmModel, completion: @escaping () -> Void) throws {
+        guard let context = appDelegate?.persistentContainer.viewContext else {
+            throw CoreDataError.contextInvalid
+        }
+        let entity = NSEntityDescription.entity(forEntityName: "Alarm", in: context)
+        
+        if let entity = entity {
+            let info = NSManagedObject(entity: entity, insertInto: context)
+            
+            info.setValue(alarm.title, forKey: "title")
+            info.setValue(alarm.id, forKey: "id")
+            info.setValue(alarm.body, forKey: "body")
+            info.setValue(alarm.date, forKey: "date")
+            info.setValue(alarm.notiType?.rawValue, forKey: "notiType")
+            
+            do {
+                try context.save()
+                completion()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+struct AlarmModel {
+    var title: String?
+    var body: String?
+    var date: Date?
+    var id: UUID?
+    var notiType: AlarmType?
+    
+    init(title: String,
+         body: String,
+         date: Date,
+         id: UUID,
+         notiType: AlarmType) {
+        self.title = title
+        self.body = body
+        self.date = date
+        self.id = id
+        self.notiType = notiType
+    }
+    
+    init?(alarm: Alarm) {
+        self.title = alarm.title
+        self.body = alarm.body
+        self.date = alarm.date
+        self.id = alarm.id
+        self.notiType = AlarmType(rawValue: Int(alarm.notiType))
+    }
+}
+
+extension AlarmModel {
+    static let sampleCoreAlarmModel: [AlarmModel] =
+    [AlarmModel(title: "첫번째 타이틀", body: "첫번째 바디", date: Date(), id: UUID(), notiType: AlarmType.couponExpiration),
+     AlarmModel(title: "2번째 타이틀", body: "2번째 바디", date: Date(), id: UUID(), notiType: AlarmType.couponExpiration),
+     AlarmModel(title: "3번째 타이틀", body: "3번째 바디", date: Date(), id: UUID(), notiType: AlarmType.couponExpiration),
+     AlarmModel(title: "4번째 타이틀", body: "4번째 바디", date: Date(), id: UUID(), notiType: AlarmType.couponExpiration),
+     AlarmModel(title: "5번째 타이틀", body: "5번째 바디", date: Date(), id: UUID(), notiType: AlarmType.couponExpiration),
+     AlarmModel(title: "6번째 타이틀", body: "6번째 바디", date: Date(), id: UUID(), notiType: AlarmType.notification),
+     AlarmModel(title: "7번째 타이틀", body: "7번째 바디", date: Date(), id: UUID(), notiType: AlarmType.notification),
+     AlarmModel(title: "8번째 타이틀", body: "8번째 바디", date: Date(), id: UUID(), notiType: AlarmType.userNotification),
+     AlarmModel(title: "9번째 타이틀", body: "9번째 바디", date: Date(), id: UUID(), notiType: AlarmType.userNotification),
+     AlarmModel(title: "10번째 타이틀", body: "10번째 바디", date: Date(), id: UUID(), notiType: AlarmType.userNotification)
+    ]
+    
+    static func addSampleData() {
+        AlarmModel.sampleCoreAlarmModel.forEach { alarm in
+            try? AlarmCoreDataManager.shared.saveData(alarm, completion: {
+                print("success")
+            })
+        }
+    }
+    
+    static func fetchSampleData() {
+        switch AlarmCoreDataManager.shared.fetchData() {
+                
+            case .success(let data):
+                print(data)
+            case .failure(_):
+                print("실행 실패")
+        }
+    }
+}
+
+enum AlarmType: Int {
+    case notification = 0 // 공지
+    case couponExpiration // 내부 알람
+    case userNotification // UserNotification 알람
+}
