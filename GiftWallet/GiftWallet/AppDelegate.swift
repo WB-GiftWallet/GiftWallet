95d3a357f0c5387accccc7f9ab7509bc45679f18
Dylan-Yoon <d.dylany21@gmail.com>
2023-05-01 21:28:50 +0900

    feat: UserNotification appPush 선택시 진입하는 View 설정

--- GiftWallet/GiftWallet/AppDelegate.swift
+++ GiftWallet/GiftWallet/AppDelegate.swift
@@ -26,14 +26,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //MARK: KakaoSDK Init
         KakaoSDK.initSDK(appKey: "16f9d943fc2e2b512f145c73a1263e39")
         
-        //MARK: Notifiaction 권한 승인 로직
-        let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            if let error = error {
-                // Handle the error here.
-                print("error: ", error)
-            }
-        }
+        //MARK: Notifiaction 권한 승인 로직 및 UserNotification Action Setting
+        configureNotification()
         
         return true
     }
@@ -81,6 +75,62 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             return [.landscapeRight]
         } else {
             return [.portrait]
+        }
+    }
+}
+
+
+// MARK: UserNotificationCenter Method
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    private func configureNotification() {
+        let application = UIApplication.shared
+        application.registerForRemoteNotifications()
+        
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+                // Handle the error here.
+                print("error: ", error)
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let application = UIApplication.shared
+        
+        if application.applicationState == .active {
+            debugPrint("Push Alarm Tap : ACTIVE")
+        }
+        
+        if application.applicationState == .inactive || application.applicationState == .background {
+            debugPrint("Push Alarm Tap : INACTIVE")
+            
+            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+            guard let window = windowScene.windows.first  else { return }
+            
+            let mainViewModel = MainViewModel()
+            let etcSettingViewModel = EtcSettingViewModel()
+            let mainTabBarController = MainTabBarController(mainViewModel: mainViewModel,
+                                                            etcSettingViewModel: etcSettingViewModel)
+            let navigationMainController = UINavigationController(rootViewController: mainTabBarController)
+            
+            window.backgroundColor = .systemBackground
+            window.rootViewController = navigationMainController
+            window.makeKeyAndVisible()
+            
+            UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: nil)
+            
+            let viewModel = AlarmListViewModel()
+            let alarmViewController = AlarmListViewController(viewModel: viewModel)
+            
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
+                //MARK: 가장 위 TopViewController GET == MainTabbarController
+                let topViewController = UIApplication.getMostTopViewController()
+                topViewController?.navigationController?.pushViewController(alarmViewController, animated: true)
+            }
         }
     }
 }
