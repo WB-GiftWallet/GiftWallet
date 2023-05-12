608dbb3c078ecbbd8df0ec2a4962bb46f0681139
woong <workplayhard1@naver.com>
2023-05-12 15:43:57 +0900

    refactor: signInWithEmail를 호출하는 LoginViewModel의 kakaoLogin()함수 순환참조 방지

--- GiftWallet/GiftWallet/LoginView/LoginViewModel.swift
+++ GiftWallet/GiftWallet/LoginView/LoginViewModel.swift
@@ -17,18 +17,18 @@ class LoginViewModel {
     func kakaoLogin(completion: @escaping () -> Void,
                     updateUserProfileCompletion: @escaping () -> Void,
                     updateDataCompletion: @escaping () -> Void) {
-        kakaoLoginManager.checkLoginEnabledAndLogin { result in
+        kakaoLoginManager.checkLoginEnabledAndLogin { [weak self] result in
             switch result {
             case .success(let user):
                 guard let userEmail = user.kakaoAccount?.email,
                       let userID = user.id?.description,
                       let userName = user.properties,
                       let userNickName = userName["nickname"] else { return }
-                self.firebaseManager.signInWithEmail(email: userEmail, password: userID) { gifts in
-                    self.firebaseManager.changeProfile(name: userNickName, completion: updateUserProfileCompletion)
+                self?.firebaseManager.signInWithEmail(email: userEmail, password: userID) { [weak self] gifts in
+                    self?.firebaseManager.changeProfile(name: userNickName, completion: updateUserProfileCompletion)
                     
                     do {
-                        try self.coreDataManager.updateAllData(gifts, completion: updateDataCompletion)
+                        try self?.coreDataManager.updateAllData(gifts, completion: updateDataCompletion)
                         
                     } catch {
                         print(error.localizedDescription)
